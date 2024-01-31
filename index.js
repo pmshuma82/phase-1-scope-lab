09516d238e322a3bd3b28989d35be79b0dace7c2
Paul M Shumate <+>
2024-01-31 09:51:26 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+let customerName = 'bob';
+
+function upperCaseCustomerName(customerName) {
+  customerName = customerName.toUpperCase();
+  returns(customerName)
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'someone';
+    leastFavoriteCustomer = 'new person';
+  
+}
+
+
+
+
